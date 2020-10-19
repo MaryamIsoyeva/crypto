9baddc0d2ab0f24348a19943cd76821165cc284f
MaryamIsoyeva <mariam1999@ukr.net>
2020-10-19 05:53:01 +0300

    main

--- cryptolab2/cryptolab2/main.cpp
+++ cryptolab2/cryptolab2/main.cpp
@@ -6,9 +6,6 @@
 #include "ECB.h"
 #include "CTR.h"
 using namespace std;
-//void m(uint32_t x[16], int a, int b, int c, int d){
-//    x[a] = 9;
-//}
 int main(int argc, const char * argv[])
 {
     unsigned char buf[5] = {'a', 'b', 'c', 'd', 'e'};
@@ -16,10 +13,10 @@ int main(int argc, const char * argv[])
     c.cipher(buf, 5);
     RC4 dc(c.key);
     dc.decipher(c.ciphered);
-    for(int z = 0; z < dc.deciphered.size(); ++z){
-        cout << dc.deciphered[z] << " ";
-    }
-    cout << "\n";
+//    for(int z = 0; z < dc.deciphered.size(); ++z){
+//        cout << dc.deciphered[z] << " ";
+//    }
+//    cout << "\n";
     
 //Test on file
     RC4 cFile("");
@@ -35,20 +32,6 @@ int main(int argc, const char * argv[])
     cFile.ciphered.clear();
     end = std::chrono::steady_clock::now();
 //    cout << "Deciphering time for RC4 " << chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "ms" << "\n";
-    
-    
-    unsigned char input[16] = {0x32, 0x43, 0xf6, 0xa8, 0x88, 0x5a, 0x30, 0x8d, 0x31, 0x31, 0x98, 0xa2, 0xe0, 0x37, 0x07, 0x34};
-    unsigned char key[16] = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c};
-
-//    uint32_t v[16] = {1, 2, 3, 4, 5, 6,7,8,8,10,11,12,13,14,15,16};
-//    for(int z = 0; z < 16; ++z){
-//        cout << v[z] << " ";
-//    }
-//    m(v, 1, 2, 3,4);
-//    cout <<"\n";
-//    for(int z = 0; z < 16; ++z){
-//        cout << v[z] << " ";
-//    }
     
     Salsa20 s;
     begin = std::chrono::steady_clock::now();
